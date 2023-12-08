aa056a77602e5e6f3e45c7a271c5dae7e81cf148
Ankit Kumar <108688582+ankitsilwal@users.noreply.github.com>
2023-12-08 11:23:40 +0530

    usermodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
-import { AuthModule } from './auth/auth.module';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { MongooseModule } from "@nestjs/mongoose";
+import { AuthModule } from "./auth/auth.module";
+import { UserModule } from "./user/user.module";
 
 @Module({
-  imports: [ConfigModule.forRoot(),
-  MongooseModule.forRoot(process.env.MONGODB_URL), AuthModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URL),
+    AuthModule,
+    UserModule,
+  ],
 })
 export class AppModule {}
